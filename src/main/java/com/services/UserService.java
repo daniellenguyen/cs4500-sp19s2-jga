4d2e24b7080a4c94bf2eeee134886af7bb733374
daniellenguyen <daniellenguyen77@gmail.com>
2019-01-23 18:51:08 -0500

    created UserService controller

---
+++ src/main/java/com/services/UserService.java
@@ -0,0 +1,10 @@
+package com.example.cs4500sp19s1jga.services;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserService {
+	static List<User> users = new ArrayList();
+
+}
